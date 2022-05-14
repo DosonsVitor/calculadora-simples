cb8178b50be61a43506eb7881abe2c32f626fb4b
Dosons <dosons123@gmail.com>
2022-05-14 20:09:33 -0300

    implementar função de raiz quadrada

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 function raiz(numX, numY){
-    var resultado;
     if(numX != false && numY != false){
         resultado = "X: " + Math.sqrt(numX) + ", Y: " + Math.sqrt(numY);
+        setResult(resultado);
     } else if(numX != false) {
-        resultado = "X: " + Map.sqrt(numX);
+        resultado = "X: " + Math.sqrt(numX);
+        setResult(resultado);
     } else if(numY != false) {
         resultado = "Y: " + Math.sqrt(numY);
-    }
+        setResult(resultado);
+    } else { document.getElementById("resultado").innerHTML = "Insira algum valor!" }
 }
