99097f6138d1b2c347d18b845d90647169ac7a13
Dosons <dosons123@gmail.com>
2022-05-14 20:15:30 -0300

    merge main into actual branch

--- script/script.js
+++ script/script.js
@@ -10,3 +10,32 @@ function raiz(numX, numY){
         setResult(resultado);
     } else { document.getElementById("resultado").innerHTML = "Insira algum valor!" }
 }
+
+function potencia(numX, numY){
+    if(numX != false && numY != false){
+        resultado = Math.pow(numX, numY);
+        setResult(resultado);
+    }
+}
+
+function getX(){ 
+    var num = document.getElementById("num1").value;
+    if(num === ""){
+        errorReport();
+        return false;
+    } else{
+        return num;
+    }
+}
+
+function getY(){ 
+    var num = document.getElementById("num2").value;
+    if(num === ""){
+        errorReport();
+        return false;
+    } else{
+        return num;
+    }
+}
+function setResult(result){ document.getElementById("resultado").innerHTML = "Resultado: " + result; }
+function errorReport(){ document.getElementById("resultado").innerHTML = "Insira os dois valores!"; }
